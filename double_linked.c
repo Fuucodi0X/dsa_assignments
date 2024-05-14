27a1d5cc428225736a197da0a6047c4eaf13092c
fuucodi0X <akmelf20@gmail.com>
2024-05-14 22:39:43 +0300

    init double linked list assignment

---
+++ double_linked.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+// Define the structure for each node in the doubly linked list
+typedef struct Node {
+int data;
+struct Node* next;
+struct Node* prev;
+} Node;
+// Function to create a new Node
+Node* createNode(int data) {
+Node* newNode = (Node*)malloc(sizeof(Node));
+if (!newNode) {
+printf("Error allocating memory!\n");
+exit(0);
+}
+newNode->data = data;
+newNode->next = NULL;
+newNode->prev = NULL;
+return newNode;
+}
+// Function to print the list
+void printList(Node* head) {
+Node* temp = head;
+printf("Forward: ");
+while (temp != NULL) {
+printf("%d ", temp->data);
+temp = temp->next;
+}
+printf("\n");
+}
